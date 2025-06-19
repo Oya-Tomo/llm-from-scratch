a29190a999dc7ff2bff96446f7d58445316b59c2
Oya-Tomo <82521012+Oya-Tomo@users.noreply.github.com>
2025-06-19 20:31:13 +0900

    update: add tokenizer utils function

--- src/tokenizer.py
+++ src/tokenizer.py
@@ -1,4 +1,15 @@
+import torch
 import tiktoken
 
 
 tokenizer = tiktoken.get_encoding("gpt2")
+
+
+def text_to_tokens(text: str, tokenizer: tiktoken.Encoding) -> torch.Tensor:
+    tokens = tokenizer.encode(text, allowed_special={"<|endoftext|>"})
+    return torch.tensor(tokens, dtype=torch.long).unsqueeze(0)
+
+
+def tokens_to_text(tokens: torch.Tensor, tokenizer: tiktoken.Encoding) -> str:
+    tokens = tokens.squeeze(0).tolist()
+    return tokenizer.decode(tokens)
